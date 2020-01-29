a77af5f8f1644f22c57fd1b64ea53d7776f573fb
Sawlani <Michael.Sawlani@us.sogeti.com>
2020-01-29 14:22:39 -0500

    connected with employees, added bootstrap, and display employees to screen

--- src/App.js
+++ src/App.js
@@ -2,25 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      Employees: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3005/getEmployees')
+      .then(Employees => Employees.json())
+      .then(Employees => {
+        console.log(Employees)
+        this.setState({
+          Employees: Employees
+        })
+      })
+  }
+
+
+  render() {
+    return (
+      <div className="App contrainer">
+        <div className="Text">
+          <h1>Video Games</h1>
+        </div>
+        <div className="row">
+          {this.state.Employees.map((Employee, index) => {
+            return (
+              <div class="card col-12 mb-4 p-2">
+                <img src={Employee.image} class="mr-3" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-text">{Employee.name}</h5>
+                  {Employee.location}
+                  <button>Click me</button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
